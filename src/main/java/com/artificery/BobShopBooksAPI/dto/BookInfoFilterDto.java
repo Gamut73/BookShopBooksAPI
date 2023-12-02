e10bce62040fdbf30ce9ebf513d9acac89ba5d56
Gamut73 <lilwaltmus@gmail.com>
2023-12-02 11:15:55 +0200

    retire the filterType enum

--- src/main/java/com/artificery/BobShopBooksAPI/dto/BookInfoFilterDto.java
+++ src/main/java/com/artificery/BobShopBooksAPI/dto/BookInfoFilterDto.java
@@ -1,6 +1,5 @@
 package com.artificery.BobShopBooksAPI.dto;
 
-import com.artificery.BobShopBooksAPI.enums.BookFilterType;
 import com.artificery.BobShopBooksAPI.enums.FilterField;
 import com.artificery.BobShopBooksAPI.enums.FilterOperator;
 import lombok.Data;
@@ -8,7 +7,6 @@ import lombok.Data;
 @Data
 public class BookInfoFilterDto {
     private FilterField name;
-    private BookFilterType type;
     private String value;
     private FilterOperator operator;
 }
--- src/main/java/com/artificery/BobShopBooksAPI/enums/BookFilterType.java
+++
@@ -1,5 +0,0 @@
-package com.artificery.BobShopBooksAPI.enums;
-
-public enum BookFilterType {
-    EQUAL, NOT_EQUAL, IS_NOT_NULL, CONTAINS, NOT_CONTAINS, GREATER_OR_EQUAL_TO
-}
